62166a3a4e91916860c7d536de3029b7ceea838b
Chris Thorn <thorn@thoughtbot.com>
2015-01-05 13:38:23 -0800

    Change same file without changing bad styles

--- dumb.rb
+++ dumb.rb
@@ -2,6 +2,11 @@ class Dumb
     def bad_spacing
       {multiple: {bad: :spacings}}
     end
+
+  def right
+    { these: { spaces: { should: { be: :ok } } } }
+  end
 end
 
 p Dumb.new.bad_spacing
+p Dumb.new.right
