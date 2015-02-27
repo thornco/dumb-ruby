0d9536017eef207d02369fba3a0e3471f5edaee1
Chris Thorn <chris@thorn.co>
2015-02-27 11:19:43 -0800

    More dumb stuff with custom config inheriting
    hmm

--- dumb.rb
+++ dumb.rb
@@ -2,6 +2,11 @@ class Dumb
   def bad_spacing
     { has: { with: { bad: :spacing } } }
   end
+
+  def more_dumb_stuff
+    { a: { hash: { with: { one_dot_nine: { syntax: { and: { long: :line } } } } } } }
+  end
 end
 
 p Dumb.new.bad_spacing
+p Dumb.new.more_dumb_stuff
