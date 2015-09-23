872023f98e195acf552987b07b1199b25f9a4e16
Chris Thorn <chris@thorn.co>
2015-09-23 13:24:57 -0700

    Dumb Python

---
+++ dumb.py
@@ -0,0 +1,6 @@
+import sys
+
+def some_really_long_function_name_that_will_hopefully_trigger_a_linter_violation_so_i_can_see_hound_python_working_in_real_life_but_like_digital_internet_life():
+    print('hi')
+
+some_really_long_function_name_that_will_hopefully_trigger_a_linter_violation_so_i_can_see_hound_python_working_in_real_life_but_like_digital_internet_life()
